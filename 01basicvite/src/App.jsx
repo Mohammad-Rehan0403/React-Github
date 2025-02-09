4c6ae30934e8ba5fece41c485816eab68fa5867d
Mohammad Rehan <mohdrehanansari95@gmail.com>
2025-02-09 23:48:39 +0530

    Working with React

--- 01basicvite/src/App.jsx
+++ 01basicvite/src/App.jsx
@@ -3,10 +3,11 @@ import Youtube from "./Youtube"
 
 
 function App() {
+  const username = "Mohammad Rehan"
   return (
     <>
-      <h1>Vite react app</h1>
-      <h1>Vite react app</h1>
+      <h1>Vite react app {3+6}</h1>
+      <h1>Vite react app {username}</h1>
       <Youtube />
     </>
   )
--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 
+const AnotherElement = (
+    <a href="https://google.com" target="_blank">visit google</a>
+)
+
+const areactElement = React.createElement(
+    'a',
+    {href: "https://google.com", target: "_blank"},
+    'click to visit google'
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
+    // areactElement
     <App />
 
 )
